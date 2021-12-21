02ae3752c18b6ef9a680dd58f6550e8b06a65077
gozixq <natsinee2545@gmail.com>
2021-12-21 13:23:54 +0700

    chewchew

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string A[9] = {" A "," B+ "," B "," C+ "," C "," D+ "," D "," F "," W "}, y;
+    cout << "Press Enter 3 times to reveal your future.";
+    int i;
+    
+    for ( i = 0; i < 3; i++)
+    {
+       cin.get();
+    }
+    
+    if (i==3)
+    {
+        srand(time(0));
+        int x = rand()%9;
+        cout << "You will get" << A[x] <<"in this 261102.";
+    }
+    
+    return 0;
+}
